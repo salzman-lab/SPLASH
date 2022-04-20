5cd941a2a6e7c954084e8411f294aab1d6284dd6
Tavor Zvi Baharav <tavorb@sh03-ln08.stanford.edu>
2022-04-19 17:27:10 -0700

    Added 4 candidate p-values to output of norm_scores.py

--- bin/norm_scores.py
+++ bin/norm_scores.py
@@ -140,6 +140,19 @@ def main():
     scores_table['expectation'] = None
     scores_table['variance_distance'] = None
 
+    #### Tavor added
+    scores_table['p-value_cj_v1'] = None
+    scores_table['p-value_cj_vGamma'] = None
+    scores_table['p-value_cj_H'] = None
+    scores_table['p-value_cj_Hv2'] = None
+    scores_table['p-value_cj'] = None
+    test_stat_cj = ( ### regenerate in case use_std is True
+            anchor_sample_scores                # df of anchor x S_j
+            .mul(pd.Series(group_ids_dict))     # multiply by group_ids
+            .mul(sqrt_n_j, axis=1)              # multiply by n_j (square-root of per anchor-sample sums)
+            .sum(axis=1)                        # sum over samples, to get a per-anchor score
+        )
+
     # for each anchor, get expectation and variance_distance
     for anchor, df in counts.groupby('anchor'):
 
@@ -188,6 +201,71 @@ def main():
         scores_table.loc[anchor, 'third_moment'] = third_moment
         scores_table.loc[anchor, 'fourth_moment'] = fourth_moment
         scores_table.loc[anchor, 'fourth_central_moment'] = fourth_central_moment
+
+        ### Tavor p-value addition
+        varEst = second_moment-first_moment**2 ### ignoring normalizing issue
+        cjs = pd.Series(group_ids_dict).to_numpy()
+        njs = n_j.loc[anchor].to_numpy() 
+        sjs = anchor_sample_scores.loc[anchor].to_numpy()[:-1] ### drop the "num_anchor_sample_scores" col
+        # s_val = norm_summary_score.loc[anchor] ### value of test statistic
+        s_val = test_stat_cj.loc[anchor]
+
+        M = njs.sum() ### total number of observations for this anchor
+        D = 8 #### maximum distance value, add as arg
+        eps = np.finfo(np.float32).eps ### to prevent division by 0
+        nj_invSqrt = 1.0/np.sqrt(njs+eps)
+        nj_invSqrt[njs==0]=0 ## set to 0
+
+
+        ### Bernstein-based concentration, p-value_cj_v1
+        term1 = 2*np.exp(-s_val**2 / 
+            (16*varEst*(cjs**2).sum() + 8/3*s_val*D*(np.abs(cjs)*nj_invSqrt).max() ))
+        term2 = 2*np.exp(-M**2*s_val**2 / 
+            (16*M*varEst*(cjs*np.sqrt(njs)).sum()**2 + 4/3*s_val*D* np.abs( (cjs*np.sqrt(njs)).sum() )))
+        term3 = np.exp(-(M-1)/(4*D**4))
+        pvalv1 = min(term1+term2+term3,1)
+
+        scores_table.loc[anchor, 'p-value_cj_v1'] = pvalv1
+
+
+        ### Bernsteion, coarsely optimizing over gamma, pvalvGamma
+        a = 8*M*(cjs**2).sum()
+        b = 8*M*s_val*D/3*(np.abs(cjs)*nj_invSqrt).max()
+        c = -2*D**4*s_val**2
+        gamma=(-b+np.sqrt(b**2-4*a*c))/(2*a)
+
+        term1g = 2*np.exp(-s_val**2 / 
+            (8*(varEst+gamma)*(cjs**2).sum() + 8/3*s_val*D*(np.abs(cjs)*nj_invSqrt).max() ))
+        term2g = 2*np.exp(-M**2*s_val**2 / 
+            (8*M*(varEst+gamma)*(cjs*np.sqrt(njs)).sum()**2 + 4/3*s_val*D* np.abs( (cjs*np.sqrt(njs)).sum() )))
+        term3g = np.exp(-(M-1)*gamma**2/(2*D**4*(varEst+gamma)))
+        pvalvGamma = min(term1g+term2g+term3g,1)
+
+        scores_table.loc[anchor, 'p-value_cj_vGamma'] = pvalvGamma
+
+
+        ### just using Hoeffdings directly with boundedness, p-value_cj_H
+        term1v2 = 2*np.exp(-s_val**2 / 
+            (8*D**2*(cjs**2).sum()))
+        term2v2 = 2*np.exp(-M*s_val**2 / 
+            (8*D**2*(cjs*np.sqrt(njs)).sum()**2 ))
+        pvalH=min(term1v2+term2v2,1)
+        scores_table.loc[anchor, 'p-value_cj_H'] = pvalH
+
+
+        ### optimized Hoeffding's bound, p-value_cj_Hv2
+        b= np.sqrt(M*(cjs**2).sum()/ (cjs*np.sqrt(njs)).sum()**2)
+        a = 1.0/(b+1)
+
+        term1Ha = 2*np.exp(- 2*(1-a)**2*s_val**2/ (D**2 * (cjs**2).sum()))
+        term2Ha = 2*np.exp( - 2*a**2*M*s_val**2 / (D**2 *(cjs*np.sqrt(njs)).sum()**2 ))
+        pvalHv2 = min(term1Ha + term2Ha,1)
+
+        scores_table.loc[anchor, 'p-value_cj_Hv2'] = pvalHv2
+
+
+        ### set overall p-value to be minimum of all candidate pvalues
+        scores_table.loc[anchor, 'p-value_cj']=min([pvalv1,pvalH,pvalvGamma,pvalHv2])
 
     # output scores table
     (
