0a6c1b905c3f2d9de8a3262f591c07eb91c7cc46
kaitlinchaung <kaitlinchaung@gmail.com>
2022-04-29 18:45:16 -0700

    add cs

--- bin/get_anchors.R
+++ bin/get_anchors.R
@@ -34,30 +34,44 @@ return_d<-function(targets, dmethod, MAXD){
 
 args <- commandArgs(TRUE)
 infile <- args[1]
-distance_type <- args[2]
-max_targets <- as.integer(args[3])
-max_dist <- as.integer(args[4])
-bonfer <- as.integer(args[5])
-pval_threshold <- as.numeric(args[6])
-outfile_scores <- args[7]
-outfile_anchors <- args[8]
+samplesheet <- args[2]
+distance_type <- args[3]
+max_targets <- as.integer(args[4])
+max_dist <- as.integer(args[5])
+bonfer <- as.integer(args[6])
+pval_threshold <- as.numeric(args[7])
+outfile_scores <- args[8]
+outfile_anchors <- args[9]
+
+
+samplesheet <- fread(samplesheet, header=F)
+
+if (ncol(samplesheet) == 1) {
+    ## if there is only one column of files, use_c is set to false
+    use_c = FALSE
+
+} else if (ncol(samplesheet) == 2) {
+    ## if there is a second column, use_c is set to true
+    use_c = TRUE
+
+    colnames(samplesheet) <- c('sample', 'cs')
+    ## create sample column for joining later
+    samplesheet[, sample := sub('.fastq.gz', '', basename(sample))]
+}
 
 m = fread(infile, fill=T, header=F)
 names(m) = c("counts", "seq", "sample")
 
 m[ ,anchor := substr(seq,1,27)]
 m[ ,target := substr(seq,28,54)]
-m[ ,cs := 1] ## needs to be merged with a real table CS ARE CONSTANTS IN THE STATISTIC
 m[ ,n.targ := length(unique(target)),by=anchor]
 m = m[n.targ>1] # JUST COMPUTE FOR MORE THAN 1 TARGET
 m[ ,target.count := sum(counts), by=anchor] # gives the count per target for each anchor
 
-print('starting loop')
-print(length(unique(m$anchor)))
 i = 0
 for (anch in unique(m$anchor)){ ## INEFFICIENT can be done with lapply
     i = i + 1
-    print(i)
+
     ll=length(unique(m[anchor== anch][order(-counts)]$target)) # TOTAL NUM TARGETS
 
     ## GENERATES LIST OF TARGETS ONLY FOR THE TOP max_targets
@@ -74,7 +88,6 @@ for (anch in unique(m$anchor)){ ## INEFFICIENT can be done with lapply
     if ( sum(ls() %like% "tomerge.distances")>0){ tomerge.distances=rbind(tomerge.distances,into)}
     if ( sum(ls() %like% "tomerge.distances")==0){ tomerge.distances= into}  ## keeps a
 }
-print('done loop')
 
 ######################################################
 ## CREATE THE KEY TO MERGE TARGET DISTANCES ON ON
@@ -94,37 +107,52 @@ new[ ,num.sample:= length(unique(sample)), by=anchor]
 new[ ,M:=sum(counts),by=anchor]
 
 ## create a new list of cs by sample by anchor has all the data
-compute.a = unique( new[,list(cs, anchor, sample, M, score_per_sample, n_j, mu)])
+compute.a = unique( new[,list(anchor, sample, M, score_per_sample, n_j, mu)])
 ## define new c per anchor then mergein
 
-pv = matrix(nrow=dim(compute.a)[1],ncol=bonfer)
 
-print('starting correction')
+if (use_c == FALSE){
 
-for (j in 1:bonfer){ # 5 projections of cs
-    cs = runif(dim(compute.a)[1],-1,1) # generates newcs in the next line by first removing current cs
-    compute.a[ ,cs:=NULL]; compute.a=cbind(compute.a,cs)
-    compute.a[ ,anchor_score := sum(cs*(score_per_sample)),by=anchor] #
+    pv=matrix(nrow=dim(compute.a)[1],ncol=bonfer)
 
+    for (j in 1:bonfer){ # 5 projections of cs
+        cs=runif(dim(compute.a)[1],-1,1) # generates newcs in the next line by first removing current cs
+        compute.a[,cs:=NULL]; compute.a=cbind(compute.a,cs)
+        compute.a[,anchor_score :=  sum(cs*(score_per_sample)),by=anchor] #
+        ## CONSTANTS NEEDED FOR BOTH TERMS
+        compute.a[,a:= 1/(1+sqrt(M*sum(cs^2) /(sum(cs*sqrt(n_j))^2 ))) ,by=anchor]
+        compute.a[,sumcsq:=sum(cs^2),by=anchor]
+        compute.a[,sumc_sqrtnj := sum( cs*sqrt(n_j)),by=anchor]
+        ## since anchorscore is squared, no need for abs value
+        compute.a[, pterm1:= 2*(exp(- (2*(1-a)^2*anchor_score^2/(max_dist^2*sumcsq))))]
+        compute.a[, pterm2:= 2*(exp(- ((2*a^2*anchor_score^2*M)/(max_dist^2 *sumc_sqrtnj^2)))) ]
+
+        pv[,j]= bonfer*(compute.a$pterm1+compute.a$pterm2)
+    }
+
+    ## take the min across the 5 already BH-corrected values
+    bf.cor.p=apply(pv, 1, min)
+    compute.a=cbind(compute.a, bf.cor.p)
+
+} else {
+
+    compute.a=merge(compute.a, samplesheet, by="sample",all.x=T)# OUTER JOIN, assign 0 to cs 0
+    compute.a=compute.a[is.na(cs),cs:=0]
+
+    compute.a[,anchor_score :=  sum(cs*(score_per_sample)),by=anchor] #
     ## CONSTANTS NEEDED FOR BOTH TERMS
-    compute.a[ ,a:= 1/(1+sqrt(M*sum(cs^2) /(sum(cs*sqrt(n_j))^2 ))) ,by=anchor]
-    compute.a[ ,sumcsq:=sum(cs^2),by=anchor]
-    compute.a[ ,sumc_sqrtnj := sum( cs*sqrt(n_j)),by=anchor]
-
+    compute.a[,a:= 1/(1+sqrt(M*sum(cs^2) /(sum(cs*sqrt(n_j))^2 ))) ,by=anchor]
+    compute.a[,sumcsq:=sum(cs^2),by=anchor]
+    compute.a[,sumc_sqrtnj := sum( cs*sqrt(n_j)),by=anchor]
     ## since anchorscore is squared, no need for abs value
     compute.a[, pterm1:= 2*(exp(- (2*(1-a)^2*anchor_score^2/(max_dist^2*sumcsq))))]
     compute.a[, pterm2:= 2*(exp(- ((2*a^2*anchor_score^2*M)/(max_dist^2 *sumc_sqrtnj^2)))) ]
-
-    pv[,j]= bonfer*(compute.a$pterm1+compute.a$pterm2)
+    compute.a[,bf.cor.p:=pterm1+pterm2] ## NO BF CORRECTION
 }
-print('done correction')
 
-## take the min across the 5 already BH-corrected values
-# Kaitlin, you could change the pvalue threshold for the purpose of testing
-compute.a = cbind(compute.a, apply(pv,1,min))
 
-# anchors = compute.a[V2<pval_threshold]
-anchors = head(compute.a[order(compute.a$V2, decreasing=T), "anchor"], 1000)
+# anchors = compute.a[pval<pval_threshold]
+anchors = head(compute.a[order(compute.a$bf.cor.p, decreasing=T), "anchor"], 1000)
 
 write.table(compute.a, outfile_scores, col.names=T, row.names=F, quote=F, sep='\t')
 write.table(anchors, outfile_anchors, col.names=F, row.names=F, quote=F, sep='\t')
