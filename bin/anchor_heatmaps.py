84fc49058f4ed66ed87927e14c7f522c16583c2d
Elisabeth Meyer <eameyer@sh02-ln04.stanford.edu>
2022-09-15 15:22:10 -0700

    removing print statement

--- bin/anchor_heatmaps.py
+++ bin/anchor_heatmaps.py
@@ -711,7 +711,6 @@ def main():
     #### generate contingency tables from abundant_stratified_anchors files
     print('regenerating anchor target counts file from scratch')
     ctsDf = (constructCountsDf(anchLst, args).set_index(['anchor','target']))
-    print(ctsDf.head(2))  # DELETE LATER
     for samp in sampleNames:
         if samp not in ctsDf.columns:
             ctsDf[samp] = 0
