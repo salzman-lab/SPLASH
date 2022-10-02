1cb45104c3c3946811857cd03abe357eecaa9128
Tavor Baharav <tavorb@stanford.edu>
2022-10-01 20:01:16 -0700

    Tightened p-value bound, separated p-value bound and utility functions into stats_utility.py, and data loading functionality into compute_spectral_pvals.py, with a wrapper script in spectral_pvals_wrapper.py. Should work as is for no metadata, tuning still required for samplesheet-based

---
+++ bin/spectral_pvals_wrapper.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm,tqdm_notebook, tqdm_pandas
+import os
+import glob
+import pickle
+import argparse
+import scipy
+import scipy.stats
+import statsmodels.api as sm
+from pathlib import Path 
+import itertools
+
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument( ### folder with abundant_stratified_{}.txt.gz files
+        "--in_folder",
+        type=str
+    )
+    parser.add_argument( ### folder containing intermediate p-value files
+        "--intermediate_fldr",
+        type=str
+    )
+    parser.add_argument( ### output file path, required
+        "--outfile_scores",
+        type=str
+    )
+    parser.add_argument( ### samplesheet file, if samplesheet-based cj (metadata) are to be used
+        "--samplesheet",
+        type=str,
+        default=""
+    )
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+
+    print('running main')
+    for f_seq in itertools.product(['A','T','C','G'],repeat=3):
+        f_seq = "".join(f_seq)
+        command = """python /oak/stanford/groups/horence/tavorb/stringstats_tavor/stringstats/bin/compute_spectral_pvals.py --outfile_scores {}/int_pvals_{}.tsv --samplesheet {} --infile {}/abundant_stratified_{}.txt.gz""".format(args.intermediate_fldr,f_seq
+        ,args.samplesheet,
+        args.in_folder,f_seq)
+        print("running: ", command)
+        os.system(command)
+        break
+
+    print('aggregating')
+    dfs = []
+    for df_path in glob.glob(args.intermediate_fldr+"/*.tsv"):
+        try:
+            dfs.append(pd.read_csv(df_path.strip(), sep='\t'))
+        except pd.errors.EmptyDataError:
+            pass
+
+
+    df = pd.concat(dfs)
+
+    ## if it's an empty df, just output the df
+    outdf = df.copy()
+
+    if not df.empty:
+
+        ### create corrected pvalues
+        newDF = outdf[outdf.columns[~outdf.columns.str.startswith('pval')]]
+
+        for c in outdf.columns[outdf.columns.str.startswith('pval')]:
+            _, c_corrected, _, _ = sm.stats.multipletests(outdf[c], method='fdr_by')
+            newDF[c+'_corrected'] = c_corrected
+            
+        newDF.sort_values('pval_SVD_corrAnalysis_corrected').to_csv(args.outfile_scores, sep='\t', index=False)
+
+
+
+
+
+main()
